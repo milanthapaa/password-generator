5992b53d5f0b85d1c353e52a199d3a1785ba6c1a
Milan Thapa <milanthapa299@gmail.com>
2022-06-03 18:27:50 +0545

    added necessary character

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import string
 
 
 def generate_password():
-    pass
+    characters = list(string.ascii_lowercase)
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    special_characters = ['!', '@', '#', '$', '%', '^', '&', '*']
 
 
 if __name__ == "__main__":
